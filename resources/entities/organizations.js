b92c03d1e8c697e299439e4f6d1b5524bc03b189
Felix Sasaki <felix.sasaki@gmail.com>
2021-09-20 09:38:50 +0200

    Update organizations.js

--- resources/entities/organizations.js
+++ resources/entities/organizations.js
@@ -1,18 +1,33 @@
 
 const Organizations = { template: `
-  <kgtemplate geospecificqueryid=""
-  generalqueryid="organizationsGeneralQuery"
-  entityspecificqueryid="organizationsSpecificQuery"
-  entity="organizations"
-  relatedentity1="tools"
-  relatedEntity1Type="url"
-  relatedentity2="persons"
-  relatedEntity2Type="url"
-  outputlocation="true"
-  message1="outputTemplates.TC3""
-  message2="outputTemplates.TC3Details"
+  <kgtemplate
+  v-bind:generalqueryid="this.config.generalqueryid"
+  v-bind:entityspecificqueryid="this.config.entityspecificqueryid"
+  v-bind:geospecificqueryid="this.config.mygeospecificqueryid"
+  v-bind:entity="this.config.entity"
+  v-bind:relatedentity1="this.config.relatedEntity1"
+  v-bind:relatedEntity1Type="this.config.relatedEntity1Type"
+  v-bind:relatedentity2="this.config.relatedEntity2"
+  v-bind:relatedEntity2Type="this.config.relatedEntity2Type"
+  v-bind:outputlocation="this.config.outputlocation"
+  v-bind:message1="this.config.message1"
+  v-bind:message2="this.config.message2"
   ></kgtemplate>
   `,data: function () {
-    return { }
+    return {
+      config : {
+        generalqueryid : "organizationsGeneralQuery",
+        entityspecificqueryid : "organizationsSpecificQuery",
+        entity : "organizations",
+        relatedEntity1 : "tools",
+        relatedEntity2 : "persons",
+        relatedEntity1Type : "url",
+        relatedEntity2Type : "url",
+        outputlocation : "true",
+        message1 : "outputTemplates.TC3",
+        message2 : "outputTemplates.TC3Details",
+        mygeospecificqueryid : ""
+      }
+     }
   }
 }
